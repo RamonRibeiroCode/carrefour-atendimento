92cf32ce0c00294c6bf35d494ac9783cf20b02c0
Ramon Ribeiro <ramon_ramos_ext@carrefour.com>
2023-09-07 22:16:59 -0300

    feat: first commit

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,62 +1,54 @@
-import { Configuration, OpenAIApi } from "openai";
+import OpenAI from "openai";
+import { context } from "../mocks/context";
 
-const configuration = new Configuration({
+const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
-const openai = new OpenAIApi(configuration);
 
 export default async function (req, res) {
   if (!configuration.apiKey) {
     res.status(500).json({
       error: {
-        message: "OpenAI API key not configured, please follow instructions in README.md",
-      }
+        message:
+          "OpenAI API key not configured, please follow instructions in README.md",
+      },
     });
+
     return;
   }
 
-  const animal = req.body.animal || '';
-  if (animal.trim().length === 0) {
-    res.status(400).json({
-      error: {
-        message: "Please enter a valid animal",
-      }
-    });
-    return;
-  }
+  const animal = req.body.animal || "";
 
   try {
-    const completion = await openai.createCompletion({
+    const completion = await openai.c({
       model: "text-davinci-003",
       prompt: generatePrompt(animal),
-      temperature: 0.6,
+      temperature: 0,
+      max_tokens: 300,
     });
+
     res.status(200).json({ result: completion.data.choices[0].text });
-  } catch(error) {
-    // Consider adjusting the error handling logic for your use case
+  } catch (error) {
     if (error.response) {
       console.error(error.response.status, error.response.data);
       res.status(error.response.status).json(error.response.data);
     } else {
       console.error(`Error with OpenAI API request: ${error.message}`);
+
       res.status(500).json({
         error: {
-          message: 'An error occurred during your request.',
-        }
+          message: "An error occurred during your request.",
+        },
       });
     }
   }
 }
 
-function generatePrompt(animal) {
-  const capitalizedAnimal =
-    animal[0].toUpperCase() + animal.slice(1).toLowerCase();
-  return `Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: ${capitalizedAnimal}
-Names:`;
+function generatePrompt(question) {
+  return `Answer the question based on the context below, 
+          and if the question can't be answered based on the context, 
+          say \"I don't know\"\n\n\"\n\n
+          Context: ${context}\n\n---\n\n
+          Question: ${question}\n
+          Answer:`;
 }
---
+++ pages/mocks/context.js
@@ -0,0 +1,111 @@
+export const context = `
+  Como faço para cancelar minha compra feita no site?
+  Para cancelar um pedido, vendido pelo Carrefour no site, siga os passos abaixo:
+  Importante: Você pode solicitar o cancelamento de pedidos que ainda não entraram no processo de entrega. Se o seu produto já foi recebido e estiver dentro do prazo de 7 dias após a entrega, você pode solicitar a devolução do pedido.
+  Acesse Meus Pedidos
+  Depois de localizar o pedido que você deseja cancelar, clique em "Ver detalhes do pedido"
+  Clique no botão "Cancelar pedido"
+  Para cancelar produtos vendidos por lojas parceiras no site do Carrefour, ou outras opções de cancelamento, clique aqui para entrar em contato com nosso atendimento.
+  
+  Como acompanhar a entrega do meu pedido?
+  Para acompanhar a entrega de um pedido, siga os passos abaixo:
+  Acesse Meus Pedidos
+  Depois de localizar o pedido que você deseja acompanhar, clique em "Ver detalhes do pedido"
+  Verifique o atual "Status do Pedido" para ter uma ideia do andamento da entrega
+  Para algumas transportadoras, disponibilizamos o botão "Rastrear pedido". Clique nele (se estiver disponível) para obter mais informações sobre a localização do seu pedido.
+  
+  Como eu fico sabendo se meu pedido / pagamento foi aprovado?
+  Assim que você finalizar a compra em nosso site, enviaremos um e-mail de confirmação do pedido. Quando o pagamento for efetuado e confirmado, você receberá um segundo e-mail informando que seu pedido está sendo preparado.
+  Além dos e-mails, também é possível acompanhar se o pedido / pagamento foi aprovado em:
+  Acesse Meus Pedidos
+  Depois de localizar o pedido que você deseja acompanhar, clique em "Ver detalhes do pedido"
+  Verifique o atual "Status do Pedido"
+  
+  Quais são os meios de pagamento disponíveis?
+  Para tornar suas compras no site mais práticas e fáceis, oferecemos diversas opções de pagamento:
+  PIX
+  Cartão de crédito
+  Cartão de crédito Amex
+  Cartão de débito
+  Cartão Carrefour
+  Dois cartões diferentes (incluindo o Cartão Carrefour)
+  Boleto bancário
+  Cartão presente
+  Vale-troca
+  Importante: Você pode combinar quantos cartões presentes e vale-trocas quiser para complementar o seu pagamento, exceto no caso de pagamento por boleto bancário.
+  Esqueci minhas informações de acesso do Cartão Carrefour
+  Se você esqueceu sua senha ou informações de acesso, basta você acessar www.carrefoursolucoes.com.br e usar a opção 'Esqueceu sua senha' disponível na tela de login para recuperar sua senha.Você precisará ter acesso ao e-mail cadastrado para completar este processo. Caso não tenha mais acesso ao e-mail, utilize nosso Fale Conosco e solicite a limpeza do seu cadastro.
+  Onde posso visualizar minha fatura ou solicitar segunda via?
+  Para obter uma nova via de sua fatura ou solicitara segunda via, acesse a Área do Cliente no site do Cartão Carrefour e selecione as opções Faturas > Fatura do Cartão > Fatura Completa em PDF.
+  Se preferir, você também por se cadastrar para receber sua fatura por e-mail. Basta acessar as opções Faturas > Fatura por e-mail. Após o cadastramento da fatura por e-mail é importante lembrar que você não receberá mais a fatura por correio.
+  Você pode solicitar sua segunda via através do app Cartão Carrefour ou do Whatsapp, mediante cadastro do celular seguro.
+  Como devolver um pedido e ser reembolsado?
+  Para devolver um pedido, vendido pelo Carrefour no site, siga os passos abaixo:
+  Importante: Você pode solicitar a devolução de um pedido, desde que esteja dentro do prazo de 7 dias, a partir da data de entrega realizada.
+  Acesse Meus Pedidos
+  Depois de localizar o pedido que você deseja devolver, clique em "Ver detalhes do pedido"
+  Clique no botão "Devolução"
+  Para devolver produtos vendidos por lojas parceiras no site do Carrefour, ou outras opções de devolução, clique aqui para entrar em contato com nosso atendimento.
+  Como eu visualizo a Nota Fiscal do meu pedido?
+  A Nota Fiscal de um pedido é gerada na etapa em que começamos a preparar os produtos para a entrega.
+  É importante ressaltar que a confirmação do pagamento é necessária para que isso ocorra.
+  Não se preocupe, enviaremos um e-mail assim que a Nota Fiscal for gerada.
+  Caso a Nota Fiscal já tenha sido gerada, você também pode fazer o download acessando:
+  Acesse Meus Pedidos
+  Depois de localizar o pedido que você deseja visualizar a Nota Fiscal, clique em "Ver detalhes do pedido"
+  Clique em "Download da Nota Fiscal"
+
+  Como faço para trocar o meu produto?
+  Para trocar um pedido, vendido pelo Carrefour no site, siga os passos abaixo:
+  Importante: Você pode solicitar a troca de um pedido, desde que esteja dentro do prazo de 7 dias, a partir da data de entrega realizada.
+  Fale com nosso time de atendimento clicando aqui
+  Aguarde ser atendido e digite sua solicitação
+
+
+
+
+  Ainda não recebi o meu pedido. O que devo fazer?
+  Para acompanhar a entrega de um pedido, siga os passos abaixo:
+  Acesse Meus Pedidos
+  Depois de localizar o pedido que você deseja acompanhar, clique em "Ver detalhes do pedido"
+  Verifique o atual "Status do Pedido" para ter uma ideia do andamento da entrega
+  Caso a entrega do seu pedido esteja atrasada, entre em contato com nosso time de atendimento clicando aqui.
+
+
+
+
+  Desisti da minha compra. O que devo fazer?
+  Para cancelar um pedido, vendido pelo Carrefour no site, siga os passos abaixo:
+  Importante: Você pode solicitar o cancelamento de pedidos que ainda não entraram no processo de entrega. Se o seu produto já foi recebido e estiver dentro do prazo de 7 dias após a entrega, você pode solicitar a devolução do pedido.
+  Acesse Meus Pedidos
+  Depois de localizar o pedido que você deseja cancelar, clique em "Ver detalhes do pedido"
+  Clique no botão "Cancelar pedido"
+  Para cancelar pedidos comercializados por vendedores externos no site do Carrefour, ou outras opções de cancelamento, clique aqui para entrar em contato com nosso atendimento.
+  Para devolver um pedido, vendido pelo Carrefour no site, siga os passos abaixo:
+  Importante: Você pode solicitar a devolução de um pedido, desde que esteja dentro do prazo de 7 dias, a partir da data de entrega realizada.
+  Acesse Meus Pedidos
+  Depois de localizar o pedido que você deseja devolver, clique em "Ver detalhes do pedido"
+  Clique no botão "Devolução"
+  Para devolver produtos vendidos por lojas parceiras no site do Carrefour, ou outras opções de devolução, clique aqui para entrar em contato com nosso atendimento.
+
+
+
+
+  Qual o prazo para solicitar uma troca?
+  Você tem até 7 dias corridos após a entrega do produto para solicitar uma troca.
+  Após o prazo de 7 dias, se o produto apresentar defeito, entre em contato diretamente com o fabricante ou vendedor parceiro.
+  Para trocar um pedido, vendido pelo Carrefour no site, siga os passos abaixo:
+  Fale com nosso time de atendimento clicando aqui
+  Aguarde ser atendido e digite sua solicitação
+
+
+  Como funciona o ressarcimento de valores no site do Carrefour?
+  O ressarcimento é feito de acordo com a forma de pagamento escolhida no momento da compra:
+  Cartão de crédito ou Cartão Carrefour: o estorno será creditado na fatura e você poderá visualizar o crédito na fatura do seu cartão em até duas faturas subsequentes;
+  PIX: O estorno será imediatamente creditado na conta indicada através da chave PIX utilizada no ato da compra;
+  Boleto: o valor será transferido para a conta corrente do titular do pedido ou uma Ordem de Pagamento será gerada para que o valor seja sacado em uma agência bancária do Bradesco. A escolha depende da preferência do cliente;
+  Cartão presente: o crédito voltará para o mesmo cartão presente ou será gerado um vale-troca para usar apenas no site, dependendo da preferência do cliente e do valor em questão;
+  Vale-troca: o crédito voltará para o mesmo vale-troca ou será gerado um novo, dependendo da preferência do cliente e do valor em questão.
+  Se a sua compra foi feita com mais de um meio de pagamento, o reembolso será feito proporcionalmente de acordo com o valor pago em cada um deles.
+
+`;
